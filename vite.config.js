c783f36aa3dd02ca6f2823a4061a58977dec9873
Karl John T. Delfin <126841315+KarlDelfin@users.noreply.github.com>
2026-05-17 15:31:14 +0800

    Change base path from '/text-to-image' to '/image-to-text'

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/text-to-image',
+  base: '/image-to-text',
   build: {
     outDir: 'docs',
   },
